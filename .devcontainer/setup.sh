e3e202139e96cbdf999f6b4e753365bd09b98650
khederwkashour-kwk <khederwkashour@gmail.com>
2026-05-17 04:33:44 +0300

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 set -e
+source /opt/ros/jazzy/setup.bash
 
-# إنشاء workspace وبناء sjtu_drone
 mkdir -p /root/drone_ws/src
 cd /root/drone_ws/src
 git clone https://github.com/NovoG93/sjtu_drone.git
 
 cd /root/drone_ws
-source /opt/ros/jazzy/setup.bash
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y || true
 colcon build --symlink-install
 
-# source في bashrc
 echo "source /root/drone_ws/install/setup.bash" >> /root/.bashrc
 
-# تشغيل البيئة الرسومية في الخلفية
 nohup start-desktop.sh > /tmp/desktop.log 2>&1 &
-
-echo "✅ Setup complete! Open port 6080 to see the desktop."
+echo "✅ Setup complete!"
